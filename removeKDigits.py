d39d50e2213aebee971e11afdaac854ca382ce9f
jaineil <mandaviajaineil@gmail.com>
2020-05-13 13:11:34 +0530

    Add existing file

---
+++ removeKDigits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack=[]
+        for n in num:
+            while k and stack and stack[-1]>n:
+                stack.pop()
+                k-=1
+            stack.append(n)
+        
+        stack = stack[:-k] if k else stack
+        return "".join(stack).lstrip('0') or "0"
