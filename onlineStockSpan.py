349059794da30931c903274519c88acd4c6d4dee
jaineil <mandaviajaineil@gmail.com>
2020-05-19 13:15:02 +0530

    Add existing file

---
+++ onlineStockSpan.py
@@ -0,0 +1,17 @@
+# beautiful stack problem 
+
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price):
+        span = 1
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack.pop()[1]
+        self.stack.append([price, span])
+        return span
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
