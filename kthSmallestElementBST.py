0f8c3f784b79e77e5ebfff5e6b1d8a2dae4de013
jaineil <mandaviajaineil@gmail.com>
2020-05-20 13:16:52 +0530

    Add existing file

---
+++ kthSmallestElementBST.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Inorder traversal of the tree will give us elements in ascending order
+# We simply need to return the 'kth' element of inorder traversal
+
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        def inorder(r):
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+    
+        return inorder(root)[k - 1]
